e34782807791ce09d5fed23eb530c54f01052096
dc5v <dev@dc5v.com>
2024-07-31 18:45:22 +0900

    add parameters

---
+++ parameters.hpp
@@ -0,0 +1,9 @@
+#ifndef PARAMETERS_HPP
+#define PARAMETERS_HPP
+
+#define MODEL_FILENAME "randomforest-image-train.model"
+#define GAUSSIAN_SIZE 5
+#define FOREST_DEPTH 10
+#define FOREST_TREES 5
+
+#endif
